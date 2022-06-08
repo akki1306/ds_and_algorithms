e841cf72cd6c8b4c8a9a74b392d4f626cdb1e527
Akshay Kulkarni <akshayk.gj@gmail.com>
2022-06-08 09:30:35 +0530

    Pairs to target sum

--- arrays/Pairs.java
+++ arrays/Pairs.java
@@ -6,7 +6,7 @@ import java.util.*;
  * Given an array containing N integers, and an Number S denoting a target sum.
  *
  * Find two distinct integers that can pair up to form target sum.
- * Let us assume that there will be only one such pair.
+ * Assume that there will be only one such pair.
  *
  * Input
  * array=[10,5,2,3,-6,9,11]
