2d3b1e8fb577c2ab3eaf5d92cb990cffcc541971
Akshay Kulkarni <akshayk.gj@gmail.com>
2022-06-25 22:49:11 +0530

    rotated search

---
+++ binarysearch/RotatedSearch.java
@@ -0,0 +1,33 @@
+package binarysearch;
+
+public class RotatedSearch {
+    public static void main(String[] args) {
+        System.out.println(rotatedSearch(new int[]{7, 9, 10, 1, 2, 3, 4, 5, 6}, 10));
+    }
+
+    private static int rotatedSearch(int[] arr, int element) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == element)
+                return mid;
+            else if (arr[0] <= arr[mid]) {
+                if (element <= arr[mid] && element >= arr[start])
+                    end = mid - 1;
+                else {
+                    start = mid + 1;
+                }
+            } else {
+                if (element >= arr[mid] && element <= arr[end])
+                    start = mid + 1;
+                else{
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
