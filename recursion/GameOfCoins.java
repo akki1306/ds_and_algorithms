2168522bdcb0ff20407a5f725b2e19415bc98c42
Akshay Kulkarni <akshayk.gj@gmail.com>
2022-06-28 16:28:28 +0530

    refactor to use recurrence relation

--- recursion/GameOfCoins.java
+++ recursion/GameOfCoins.java
@@ -1,31 +1,35 @@
 package recursion;
 
+/**
+ * Oxford and Henry are playing the game of coins. The have a row of 'n' coins [C1,C2,C3...Cn] with values [V1,V2,V3...Vn]
+ * Where Ci coin has Vi value. They take turns alternatively. In one turn the player can pick either the first or the last
+ * coin of the row. Given both Oswald and Henry are very smart players, you need to find the maximum possible value
+ * Oswald can earn if he plays first.
+ * <p>
+ * Input:
+ * <p>
+ * array of coins which represent value of each coin at that position
+ * <p>
+ * Output:
+ * <p>
+ * Return a single integer which is the maximum possible value as asked in the question
+ * <p>
+ * Constraints:
+ * 1<=N<=15
+ */
 public class GameOfCoins {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4};
-        System.out.println(gameOfCoins(arr, 0, 0, arr.length - 1));
+        System.out.println(gameOfCoins(arr, 0, arr.length - 1));
     }
 
-    private static int gameOfCoins(int[] arr, int round, int i, int j) {
-        if (i == j)
-            if (round % 2 == 0)
-                return arr[i];
-            else
-                return 0;
+    private static int gameOfCoins(int[] arr, int i, int j) {
+        if (i >= arr.length || j <= -1 || i > j)
+            return 0;
 
-        boolean isGreater = arr[i] > arr[j];
-        int val = Math.max(arr[i], arr[j]);
-
-        if (isGreater)
-            i++;
-        else
-            j--;
-
-        int nextRoundVal = gameOfCoins(arr, round + 1, i, j);
-
-        if (round % 2 == 0)
-            return val + nextRoundVal;
-        else
-            return nextRoundVal;
+        // recurrence relation
+        int option1 = arr[i] + Math.min(gameOfCoins(arr, i + 2, j), gameOfCoins(arr, i + 1, j - 1));
+        int option2 = arr[j] + Math.min(gameOfCoins(arr, i + 1, j - 1), gameOfCoins(arr, i, j - 2));
+        return Math.max(option1, option2);
     }
 }
