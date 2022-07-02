9228137415661c4c5ad7a3e2acc28beb6af86085
Akshay Kulkarni <akshayk.gj@gmail.com>
2022-07-02 11:57:56 +0530

    build tree from level order traversal

---
+++ binarytrees/BuildLevelOrder.java
@@ -0,0 +1,39 @@
+package binarytrees;
+
+import binarytrees.ds.Node;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class BuildLevelOrder {
+    public static void main(String[] args) {
+        Node root = buildLevelOrder(new int[]{1, 2, 3, 4, 5, -1, 6, -1, -1, 7, -1, -1, -1, -1, -1});
+        System.out.println();
+    }
+
+    // 1 2 3 4 5 -1 6 -1 -1 7 -1 -1 -1 -1 -1
+    private static Node buildLevelOrder(int[] arr) {
+        Queue<Node> queue = new LinkedBlockingQueue<>();
+
+        Node root = new Node(arr[0], null, null);
+
+        queue.add(root);
+        int i=0;
+        while (!queue.isEmpty() && i < arr.length) {
+            Node n = queue.poll();
+            ++i;
+            if (i< arr.length && arr[i] != -1) {
+                n.left = new Node(arr[i], null, null);
+                queue.add(n.left);
+            }
+
+            ++i;
+            if (i < arr.length && arr[i] != -1) {
+                n.right = new Node(arr[i], null, null);
+                queue.add(n.right);
+            }
+        }
+
+        return root;
+    }
+}
