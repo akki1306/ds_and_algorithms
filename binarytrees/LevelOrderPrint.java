11cbf0e11165fd7e68c25481424d99e1a25572f3
Akshay Kulkarni <akshayk.gj@gmail.com>
2022-07-02 10:59:13 +0530

    level order traversal

---
+++ binarytrees/LevelOrderPrint.java
@@ -0,0 +1,35 @@
+package binarytrees;
+
+import binarytrees.ds.Node;
+import binarytrees.ds.Tree;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class LevelOrderPrint {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        levelOrderPrint(tree.buildTree(new int[]{1, 2, 4, -1, -1, 5, 7, -1, -1, -1, 3, -1, 6, -1, -1}));
+    }
+
+    private static void levelOrderPrint(Node root) {
+        Queue<Node> queue = new LinkedBlockingQueue<>();
+        queue.add(root);
+        Node nullNode = new Node(-999, null, null);
+        queue.add(nullNode);
+        while (!queue.isEmpty()) {
+            Node poll = queue.poll();
+            if (poll.val == -999) {
+                System.out.println();
+                if (!queue.isEmpty())
+                    queue.add(nullNode);
+            } else
+                System.out.print(poll.val + " ");
+
+            if (poll.left != null)
+                queue.add(poll.left);
+            if (poll.right != null)
+                queue.add(poll.right);
+        }
+    }
+}
