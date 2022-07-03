508f3cae87f6e6313820278162151b092d98bc61
akshayk.gj@gmail.com <Akshayk@13>
2022-07-03 11:49:46 +0530

    max subset sum tree

---
+++ binarytrees/MaxSubsetSumTree.java
@@ -0,0 +1,41 @@
+package binarytrees;
+
+import binarytrees.ds.Node;
+
+public class MaxSubsetSumTree {
+    public static void main(String[] args) {
+        Node root = BuildLevelOrder.buildLevelOrder(new int[]{1, 2, 3, 4, 5, 6, -1, -1, -1, 7, -1});
+        IEPair iePair = maxSubsetSumTree(root);
+        System.out.println(Math.max(iePair.included, iePair.excluded));
+    }
+
+    private static IEPair maxSubsetSumTree(Node node) {
+        if (node == null)
+            return new IEPair(0, 0);
+
+        IEPair left = maxSubsetSumTree(node.left);
+        IEPair right = maxSubsetSumTree(node.right);
+        int nodeIncluded = node.val + left.excluded + right.excluded;
+        int nodeExcluded = Math.max(left.included, left.excluded) + Math.max(right.included, right.excluded);
+        IEPair ie = new IEPair(nodeIncluded, nodeExcluded);
+        return ie;
+    }
+
+    static class IEPair {
+        int included;
+        int excluded;
+
+        IEPair(int included, int excluded) {
+            this.included = included;
+            this.excluded = excluded;
+        }
+
+        @Override
+        public String toString() {
+            return "IEPair{" +
+                    "included=" + included +
+                    ", excluded=" + excluded +
+                    '}';
+        }
+    }
+}
