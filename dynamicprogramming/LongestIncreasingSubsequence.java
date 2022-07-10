4d5851824ff2093ee89cc736debbce46480f5f74
Akshay Kulkarni <akshayk.gj@gmail.com>
2022-07-10 16:43:54 +0530

    longest increasing subsequence refactor

--- dynamicprogramming/LongestIncreasingSubsequence.java
+++ dynamicprogramming/LongestIncreasingSubsequence.java
@@ -3,33 +3,36 @@ package dynamicprogramming;
 public class LongestIncreasingSubsequence {
     public static void main(String[] args) {
         System.out.println(longestIncreasingSubsequenceDp(new int[]{50, 4, 10, 8, 30, 100}));
-        System.out.println(longestIncreasingSubsequenceRecursion(new int[]{50, 4, 10, 8, 30, 100}, 0));
+        System.out.println(longestIncreasingSubsequenceRecursion(new int[]{50, 4, 10, 8, 30, 100}, 5));
     }
 
     private static int longestIncreasingSubsequenceDp(int[] arr) {
         int[] dp = new int[arr.length];
 
-        dp[arr.length - 1] = 1;
-
-        for (int i = arr.length - 2; i >= 0; i--) {
-            if (arr[i] < arr[i + 1])
-                dp[i] = 1 + dp[i + 1];
-            else
-                dp[i] = dp[i + 1];
+        dp[0] = 1;
+        int lis = 1;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i])
+                    lis = Math.max(1 + dp[j], lis);
+            }
+            dp[i] = lis;
         }
-        return dp[0];
+        return dp[arr.length - 1];
     }
 
     private static int longestIncreasingSubsequenceRecursion(int[] arr, int index) {
-        if (index > arr.length - 1)
+        if (index < 0)
             return 0;
 
-        if (index == arr.length - 1)
+        if (index == 0)
             return 1;
 
-        if (arr[index] < arr[index + 1])
-            return 1 + longestIncreasingSubsequenceRecursion(arr, index + 1);
-        else
-            return longestIncreasingSubsequenceRecursion(arr, index + 1);
+        int lis = 1;
+        for (int j = 0; j < index; j++) {
+            if (arr[j] < arr[index])
+                lis = Math.max(1 + longestIncreasingSubsequenceRecursion(arr, j), lis);
+        }
+        return lis;
     }
 }
